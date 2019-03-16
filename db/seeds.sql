b0b04f771cdb7686c9355b764089a86ee44d3e74
Boki23m <boki23usa@yahoo.com>
2019-03-16 12:00:37 -0700

    git ignore added, burger.js added

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name) VALUES ('House Burger');
-INSERT INTO burgers (burger_name) VALUES ('Classic Burger');
-INSERT INTO burgers (burger_name) VALUES ('Gourmet Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('House Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Gourmet Burger', false);
