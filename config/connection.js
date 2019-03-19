4e33ff4fa8d080ac52750515c70f6897e632f220
Boki23m <boki23usa@yahoo.com>
2019-03-18 20:47:52 -0700

    post method changed

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,6 @@ var mysql = require("mysql");
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  // DB is JawsDB on Heroku
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   // DB is local on localhost
